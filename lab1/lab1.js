8c2e3ba4ae746fdebd63e3be448d0ea9276e4784
caoyudong <cydjohn@live.com>
2016-09-13 20:51:45 -0400

    init

---
+++ lab1/lab1.js
@@ -0,0 +1,11 @@
+// For your first function, you will calculate the sum of the squares 
+// of 3 numbers and return that result. That means sumOfSquares(5, 3, 10)
+//  would return 134.
+sumOfSquares(num1, num2, num3)
+
+
+
+sayHelloTo(firstName, lastName, title)
+cupsOfCoffee(howManyCups)
+occurrencesOfSubstring(fullString, substring)
+randomizeSentences(paragraph)
