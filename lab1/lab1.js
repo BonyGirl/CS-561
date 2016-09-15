d529e6d150fa37e936db6ad703d66de342c12e6b
caoyudong <cydjohn@live.com>
2016-09-15 14:28:56 -0400

    sumOfSquares

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,11 +1,37 @@
 // For your first function, you will calculate the sum of the squares 
 // of 3 numbers and return that result. That means sumOfSquares(5, 3, 10)
 //  would return 134.
-sumOfSquares(num1, num2, num3)
+
+function sumOfSquares(num1, num2, num3) {
+    if (num1 === undefined || typeof num1 !== "number") {
+        throw "num1 is not a number";
+    }
+    if (num2 === undefined || typeof num2 !== "number") {
+        throw "num2 is not a number";
+    }
+    if (num3 === undefined || typeof num3 !== "number") {
+        throw "num3 is not a number";
+    }
+
+    return num1*num1+num2*num2+num3*num3;
+}
+
+// function sayHelloTo(firstName, lastName, title){
+
+// }
+// function cupsOfCoffee(howManyCups){
+
+// }
+// function occurrencesOfSubstring(fullString, substring){
+
+// }
+// function randomizeSentences(paragraph){
+
+// }
+
+const sumOfSquaresAnswer = sumOfSquares(5,3,10);
+console.log( "the sum of 5, 3, 10 squares is: " ,sumOfSquaresAnswer);
 
 
 
-sayHelloTo(firstName, lastName, title)
-cupsOfCoffee(howManyCups)
-occurrencesOfSubstring(fullString, substring)
-randomizeSentences(paragraph)
+// console.log("hello!!");
