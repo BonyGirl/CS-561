40f540d439d7543e6aa2ed2281c642915665f575
caoyudong <cydjohn@live.com>
2016-10-03 10:09:14 -0400

    update

--- lab3/todo.js
+++ lab3/todo.js
@@ -40,43 +40,25 @@ let exportedMethods = {
                     return this.getTask(newId);
                 });
         });
-    // },
-    // removeDog(id) {
-    //     if (!id) 
-    //         return Promise.reject("You must provide an id to search for");
+    },
+    removeTask(id) {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
         
-    //     return dogs().then((dogCollection) => {
-    //         return dogCollection
-    //             .removeOne({_id: id})
-    //             .then((deletionInfo) => {
-    //                 if (deletionInfo.deletedCount === 0) {
-    //                     throw(`Could not delete dog with id of ${id}`)
-    //                 }
-    //             });
-    //     });
-    // },
-    // updateDog(id, name, breeds) {
-    //     if (!id) 
-    //         return Promise.reject("You must provide an id to search for");
+        return todo().then((todoCollection) => {
+            return todoCollection
+                .removeOne({_id: id})
+                .then((deletionInfo) => {
+                    if (deletionInfo.deletedCount === 0) {
+                        throw(`Could not delete task with id of ${id}`)
+                    }
+                });
+        });
+    },
+    completeTask(id) {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
         
-    //     if (!breeds || !Array.isArray(breeds)) 
-    //         return Promise.reject("You must provide an array of breeds");
-        
-    //     if (breeds.length === 0) 
-    //         return Promise.reject("You must provide at least one breed.");
-        
-    //     return dogs().then((dogCollection) => {
-    //         let updatedDog = {
-    //             name: name,
-    //             breeds: breeds
-    //         };
-
-    //         return dogCollection.updateOne({
-    //             _id: id
-    //         }, updatedDog).then(() => {
-    //             return this.getDogById(id);
-    //         });
-    //     });
     }
 }
 
