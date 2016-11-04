4f7e0f1b9ace879cf911e134519df068c391b5e1
cydjohn <cydjohn@live.com>
2016-11-04 17:00:06 -0400

    finish Static Page

--- lab7/public/js/form.js
+++ lab7/public/js/form.js
@@ -1,39 +1,27 @@
 (function () {
-    let calculatorMethods = {
-        add: function (num1, num2) {
-            if (typeof num1 !== "number") throw "Must provide a number";
-            if (isNaN(num1)) throw "Must provide a number";
-            if (typeof num2 !== "number") throw "Must provide a number";
-            if (isNaN(num2)) throw "Must provide a number";
-
-            return num1 + num2;
-        },
-        subtract: function (num1, num2) {
-            if (typeof num1 !== "number") throw "Must provide a number";
-            if (isNaN(num1)) throw "Must provide a number";
-            if (typeof num2 !== "number") throw "Must provide a number";
-            if (isNaN(num2)) throw "Must provide a number";
-
-            return num1 - num2;
-        },
-        multiply: function (num1, num2) {
-            if (typeof num1 !== "number") throw "Must provide a number";
-            if (isNaN(num1)) throw "Must provide a number";
-            if (typeof num2 !== "number") throw "Must provide a number";
-            if (isNaN(num2)) throw "Must provide a number";
-
-            return num1 * num2;
-        },
-        divide: function (num1, num2) {
-            if (typeof num1 !== "number") throw "Must provide a number";
-            if (isNaN(num1)) throw "Must provide a number";
-            if (typeof num2 !== "number") throw "Must provide a number";
-            if (isNaN(num2)) throw "Must provide a number";
-            if (num2 <= 0) throw "Cannot divide by 0!";
-
-            return num1 / num2;
+    function insertStringIntoText(text,string,num1,num2 ){ 
+        let ans = text;
+        for(let i =1;i<=num1;i++){
+            console.log(i);
+            ans = [ans.slice(0,((i-1)*string.length+i*num2)),string,ans.slice(((i-1)*string.length+i*num2))].join('');
+            console.log(ans);
         }
-    };
+        return ans;
+
+    }
+
+    function textManipulator(text,string,num1,num2 ){
+
+        if (text === '') throw "Must provide a text";
+        if (string === '') throw "Must provide a string";
+
+        if (typeof num1 !== "number") throw "Must provide a number";
+        if (isNaN(num1)) throw "Must provide a number";
+        if (typeof num2 !== "number") throw "Must provide a number";
+        if (isNaN(num2)) throw "Must provide a number";
+
+        return insertStringIntoText(text,string,num1,num2);
+    }
 
     function operationStringToFunction(operation) {
         if (!operation) throw "No operation provided";
@@ -50,9 +38,10 @@
         // We can store references to our elements; it's better to 
         // store them once rather than re-query the DOM traversal each time
         // that the event runs.
+        var inputTextElement = document.getElementById("inputText");
+        var inputStringElement = document.getElementById("inputString");
         var firstNumberElement = document.getElementById("number1");
         var secondNumberElement = document.getElementById("number2");
-        var operationElement = document.getElementById("operation");
 
         var errorContainer = document.getElementById("error-container");
         var errorTextElement = errorContainer.getElementsByClassName("text-goes-here")[0];
@@ -66,21 +55,27 @@
             event.preventDefault();
 
             try {
+                
                 // hide containers by default
                 errorContainer.classList.add("hidden");
                 resultContainer.classList.add("hidden");
 
+
+                
                 // Values come from inputs as strings, no matter what :(
+                var textValue = inputTextElement.value;
+                var stringValue = inputStringElement.value;
                 var firstNumberValue = firstNumberElement.value;
                 var secondNumberValue = secondNumberElement.value;
-                var operationValue = operationElement.value;
+
 
                 var parsedFirstNumberValue = parseInt(firstNumberValue);
                 var parsedSecondNumberValue = parseInt(secondNumberValue);
-                var operation = operationStringToFunction(operationValue);
+                // var operation = operationStringToFunction(operationValue);
 
-                var result = operation(parsedFirstNumberValue, parsedSecondNumberValue);
-                resultTextElement.textContent = "The result is " + result;
+                // var result = operation(parsedFirstNumberValue, parsedSecondNumberValue);
+                result = textManipulator(textValue,stringValue,parsedFirstNumberValue,parsedSecondNumberValue);
+                resultTextElement.textContent = result;
                 resultContainer.classList.remove("hidden");
             } catch (e) {
                 var message = typeof e === "string" ? e : e.message;
